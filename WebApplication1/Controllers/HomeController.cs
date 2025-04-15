ab432ed267d0939349d5cd9fe788d4ffe3a7b3a1
namtht <namtht.if@gmail.com>
2025-04-15 17:47:41 +0700

    create cart

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebApplication1.Models;
+using WebApplication1.Models.ViewModel;
+using PagedList;
+using System.Data.Entity;
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        private MyStoreEntities db = new MyStoreEntities();
 
+        public ActionResult Index(string searchTerm, int? page)
+        {
+            var model = new HomeProductVM();
+            var products = db.Products.AsQueryable();
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                model.SearchTerm = searchTerm;
+                products = products.Where(p => p.ProductName.Contains(searchTerm) ||
+                    p.ProductDescription.Contains(searchTerm) || p.Category.CategoryName.Contains(searchTerm));
+            }
+            int pageNumber = page ?? 1;
+            int pageSize = 6;
+            model.FeaturedProducts = products.OrderByDescending(p => p.OrderDetails.Count()).Take(10).ToList();
+            model.NewProducts = products.OrderBy(p => p.OrderDetails.Count()).Take(20).ToPagedList(pageNumber, pageSize);
+            return View(model);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -25,6 +41,29 @@ namespace WebApplication1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult ProductDetail(int? id, int? quantity, int? page) { 
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            Product pro = db.Products.Find(id);
+            if (pro == null)
+            {
+                return HttpNotFound();
+            }
+            var products = db.Products.Where(p => p.CategoryID == pro.CategoryID && p.ProductID != pro.ProductID).AsQueryable();
+            ProductDetailsVM model = new ProductDetailsVM();
+            int pageNumber = page ?? 1;
+            int pageSize = model.PageSize;
+            model.product = pro;
+            model.RelatedProducts = products.OrderBy(p => p.ProductID).Take(8).ToList();
+            model.TopProducts = products.OrderByDescending(p => p.OrderDetails.Count()).Take(8).ToPagedList(pageNumber, pageSize);
+            if (quantity.HasValue)
+            {
+                model.quantity = quantity.Value;
+            }
+            return View(model);
         }
     }
 }
