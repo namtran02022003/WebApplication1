173254a69a39bc5d70a1348fe0a4e4322aa49578
namtht <namtht.if@gmail.com>
2025-04-15 19:07:44 +0700

    create register page

---
+++ WebApplication1/Models/ViewModel/Customer.cs
@@ -0,0 +1,27 @@
+﻿namespace WebApplication1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using WebApplication1.Models;
+
+    public partial class CustomerVM
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public CustomerVM()
+        {
+            this.Orders = new HashSet<Order>();
+        }
+
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerAddress { get; set; }
+        public string Username { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
